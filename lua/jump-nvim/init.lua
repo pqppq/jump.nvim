c4452a2fe0283ca9054e54f68f0f993014add078
pqppq <57561531+pqppq@users.noreply.github.com>
2026-05-10 11:31:56 +0900

    feat: add template

---
+++ lua/jump-nvim/init.lua
@@ -0,0 +1,6 @@
+-- creates an object for the module. all of the module's
+-- functions are associated with this object, which is
+-- returned when the module is called with `require`.
+local M = {}
+
+return M
